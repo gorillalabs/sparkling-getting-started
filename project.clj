611d5aa3872f3dda926d7df83ddaa4a88ca7e087
Christian Betz <christian.betz@performance-media.de>
2015-01-07 13:52:25 +0100

    moved to 1.0.0 of sparkling

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.6.0"]
-                           [gorillalabs/sparkling "1.0.0-SNAPSHOT"]
+                           [gorillalabs/sparkling "1.0.0"]
                            [org.clojure/tools.logging "0.3.1"]
                            [org.apache.spark/spark-core_2.10 "1.1.0" :exclusions [com.thoughtworks.paranamer/paranamer]]
                            ]
