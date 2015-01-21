aacb873bdc6b3af87df6d4891fbc45fdab1e0fe8
Christian Betz <christian.betz@performance-media.de>
2015-01-21 13:32:52 +0100

    initial project, to get going

--- project.clj
+++ project.clj
@@ -1,15 +1,18 @@
 (defproject gorillalabs/sparkling-getting-started "1.0.0-SNAPSHOT"
-            :description "A Clojure Library for Apache Spark"
-            :url "https://github.com/chrisbetz/sparkling"
+            :description "A demo project for using Spark from Clojure using gorillalabs/sparkling"
+            :url "https://github.com/gorillalabs/sparkling-getting-started"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.6.0"]
                            [gorillalabs/sparkling "1.1.0"]
                            [org.clojure/tools.logging "0.3.1"]
                            [org.apache.spark/spark-core_2.10 "1.1.0" :exclusions [com.thoughtworks.paranamer/paranamer]]
+
+                           [clj-time "0.9.0"]
+
                            ]
             :aot :all
-            :main sparkling.example.tfidf
+            :main clojured.core
 
             :javac-options ["-Xlint:unchecked" "-source" "1.6" "-target" "1.6"]
             :jvm-opts ^:replace ["-server" "-Xmx1g"]
