2d24aa9c9affeca0435448ce0430fb58bca0e518
Christian Betz <christian.betz@performance-media.de>
2015-01-23 12:11:09 +0100

    better formatting

--- test/clojured/core_test.clj
+++ test/clojured/core_test.clj
@@ -12,18 +12,19 @@
 
 (deftest test-line-count*
   (let [conf (test-conf)]
-    (spark/with-context sc conf
-                    (testing
-                      "no lines return 0"
-                      (is (= 0 (line-count* (spark/parallelize sc [])))))
+    (spark/with-context
+      sc conf
+      (testing
+        "no lines return 0"
+        (is (= 0 (line-count* (spark/parallelize sc [])))))
 
-                    (testing
-                      "a single line returns 1"
-                      (is (= 1 (line-count* (spark/parallelize sc ["this is a single line"])))))
+      (testing
+        "a single line returns 1"
+        (is (= 1 (line-count* (spark/parallelize sc ["this is a single line"])))))
 
-                    (testing
-                      "multiple lines count correctly"
-                      (is (= 10 (line-count* (spark/parallelize sc (repeat 10 "this is a single line"))))))
-                    )))
+      (testing
+        "multiple lines count correctly"
+        (is (= 10 (line-count* (spark/parallelize sc (repeat 10 "this is a single line"))))))
+      )))
 
 
