28c032aa82be2a62008cdc1a3c62d03121c8a230
Christian Betz <christian.betz@performance-media.de>
2015-01-23 12:36:55 +0100

    in practice 3 and 4: status codes

--- src/clojured/core.clj
+++ src/clojured/core.clj
@@ -26,6 +26,39 @@
         (doall result)
         result))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; In Practice 3: status codes
+
+(defn transform-log-entry [m]
+  (->
+    m
+    (update-in [:timestamp] #(tf/parse (tf/formatter "dd/MMM/yyyy:HH:mm:ss Z") %))
+    (assoc :uri (-> (:request m)
+                    (clojure.string/split #" ")
+                    (get 1)
+                    (clojure.string/split #"\?")
+                    (get 0)))))
+
+(defn parse-line [line]
+  (some->> line
+           (re-matches #"^(.*?) .*? .*? \[(.*?)] \"(.*?)\" (.*?) (.*?) \"(.*?)\" \"(.*?)\"(.*?)$")
+           rest
+           (zipmap [:ip :timestamp :request :status :length :referer :ua :duration])
+           transform-log-entry))
+
+(defn group-by-status-code [lines]
+  (->> lines
+       (map parse-line)
+       (map (fn [entry] [(:status entry) 1]))
+       (reduce (fn [a [k v]] (update-in a [k] #((fnil + 0) % v))) {})
+       (map identity)))
+
+#_ (process group-by-status-code)
+; => (["200" 261] ["404" 8])
+
+
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -39,13 +72,6 @@
               (s-conf/set-executor-env env))]
     (spark/spark-context c) ))
 
-
-(defn line-count* [lines]
-  (-> lines
-       spark/count))
-
-
-
 (defonce sc (delay (new-spark-context)))
 
 (defn process* [f]
@@ -53,14 +79,38 @@
     (f lines-rdd)))
 
 
+(defn line-count* [lines]
+  (-> lines
+       spark/count))
 
 #_
 (process line-count)
 
 #_
 (process* line-count*)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; In Practice 4: status codes cont'd
+
+(defn group-by-status-code* [lines]
+  (-> lines
+      (spark/map parse-line)
+      (spark/map-to-pair (fn [entry]
+                       (spark/tuple (:status entry) 1)))
+      (spark/reduce-by-key +)
+      (spark/map (s-de/key-value-fn vector))
+      (spark/collect)))
+
+#_
+(process* group-by-status-code*)
+
+
+
 #_
 (clojure.java.browse/browse-url "http://localhost:4040/")
+
+
 
 
 (defn -main [& args]
