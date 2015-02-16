880cd4fb3cd79b3c1fdd0069892094c878eb4640
Christian Betz <christian.betz@performance-media.de>
2015-02-16 11:40:55 +0100

    pagerank updated to sparkling 1.1.0 and clojuresque API

--- src/sparkling/example/pagerank.clj
+++ src/sparkling/example/pagerank.clj
@@ -1,5 +1,5 @@
 (ns sparkling.example.pagerank
-  (:require [sparkling.api :as spark]
+  (:require [sparkling.core :as spark]
             [sparkling.destructuring :as s-de]
             [sparkling.debug :as s-dbg]
             [sparkling.conf :as conf])
@@ -12,17 +12,16 @@
 (defn contribs [links ranks]
   (println "A step of contributions:\n  " (spark/collect links) "\n\n  " (spark/collect ranks))
   (let [contribs
-        (-> links
-            (spark/join ranks)
-            (spark/values)
-            (spark/flat-map-to-pair (s-de/val-val-fn (fn [urls, rank]
-                                                       (println (seq urls) ", " rank)
-                                                       (let [urls (seq urls)
-                                                             size (count urls)]
-                                                         (map (fn [url] (spark/tuple url, (/ rank size))) urls))))))]
+        (->> (spark/join links ranks)
+             (spark/values)
+             (spark/flat-map-to-pair (s-de/val-val-fn (fn [urls, rank]
+                                                        (println (seq urls) ", " rank)
+                                                        (let [urls (seq urls)
+                                                              size (count urls)]
+                                                          (map (fn [url] (spark/tuple url, (/ rank size))) urls))))))]
 
-    (-> (spark/reduce-by-key contribs +)
-        (spark/map-values (fn [x] (+ 0.15 (* 0.85 x)))))))
+    (->> (spark/reduce-by-key + contribs)
+         (spark/map-values (fn [x] (+ 0.15 (* 0.85 x)))))))
 
 (defn pagerank-step [iterations-to-go links ranks]
   (if (zero? iterations-to-go)
@@ -49,19 +48,19 @@
                  (spark/tuple "url4" "url3")
                  ]
 
-          links (-> (spark/parallelize-pairs sc pages)
-                    (spark/group-by-key)
-                    (spark/cache))
+          links (->> (spark/parallelize-pairs sc pages)
+                     (spark/group-by-key)
+                     (spark/cache))
           ;          _ (s-dbg/inspect links "links")
 
-          ranks (spark/map-values links (fn [_] 1.0))
+          ranks (spark/map-values (fn [_] 1.0) links)
           ;          _ (s-dbg/inspect ranks "ranks")
 
           ]
 
 
 
-      (->
+      (->>
         (pagerank-step 100 links ranks)
         spark/collect
         clojure.pprint/pprint))
