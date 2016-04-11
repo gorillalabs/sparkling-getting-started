d4a99cce1ed4b62615b01758ebcf1e0d0cfecd44
Dr. Christian Betz <cb.betz@gmail.com>
2016-04-11 10:17:45 +0200

    need to go back and check, because I cannot reproduce problems locally.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.6.0"]
-                           [gorillalabs/sparkling "1.2.4"]]
+                           [gorillalabs/sparkling "1.2.3"]]
 
             :aot [#".*" sparkling.serialization sparkling.destructuring]
             :main tf-idf.core
