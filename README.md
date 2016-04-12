4f612a5891d983d94e6db416200e4d06f6ccdeae
Kiran Karkera <kiran.karkera@gmail.com>
2016-04-12 10:59:55 +0800

    added documentation for Spark ML

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# Getting started project
+# Getting started project for [Sparkling](https://gorillalabs.github.io/sparkling/)
 
-This is a companion repo to the sparkling [getting started guide] (http://gorillalabs.github.io/sparkling/articles/getting_started.html). Please see this guide for further information.
+This is a companion repo to the sparkling [guide] (https://gorillalabs.github.io/sparkling/articles/tfidf_guide.html). Please see this guide for further information.
+
+## Support for Spark ML
+
+Support for Spark ML has been added in version 1.2.4. Please see this [Gorilla-repl worksheet](http://viewer.gorilla-repl.org/view.html?source=github&user=gorillalabs&repo=sparkling-getting-started&path=ws/classification-guide.cljw?ref=gorilla-repl) for a quick tutorial on building ML models.
+
+Alternatively, follow these steps to run the worksheet locally:
+
+* download [this repo](https://github.com/gorillalabs/sparkling-getting-started), 
+* Change to the gorilla-repl branch by entering _git checkout -b gorilla-repl_
+* run _lein gorilla_ 
+* open ws/classification-guide.cljw in the Gorilla-repl browser page for an interactive worksheet. 
--- project.clj
+++ project.clj
@@ -1,51 +1,28 @@
 (defproject gorillalabs/sparkling-getting-started "1.0.0-SNAPSHOT"
-            :description "A Clojure Library for Apache Spark"
-            :url "https://github.com/chrisbetz/sparkling"
-            :license {:name "Eclipse Public License"
-                      :url  "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.6.0"]
-                           [gorillalabs/sparkling "1.0.0"]
-                           [org.clojure/tools.logging "0.3.1"]
-                           [org.apache.spark/spark-core_2.10 "1.1.0" :exclusions [com.thoughtworks.paranamer/paranamer]]
-                           ]
-            :aot :all
-            :main sparkling.example.tfidf
+  :description "A Sample project demonstrating the use of Sparkling (https://gorillalabs.github.io/sparkling/), as shown in tutorial https://gorillalabs.github.io/sparkling/articles/tfidf_guide.html"
+  :url "https://gorillalabs.github.io/sparkling/articles/tfidf_guide.html"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [gorillalabs/sparkling "1.2.4" :exclusions [[javax.servlet/servlet-api]]]]
 
-            :profiles {:gorilla {:dependencies [[gorilla-renderable "1.0.0"]
-                                                [cider/cider-nrepl "0.8.2"]
-                                                [org.clojure/tools.nrepl "0.2.5"]
-                                                [gorilla-repl "0.3.4" :exclusions [org.clojure/clojure
-                                                                                   http-kit
-                                                                                   ring/ring-json
-                                                                                   cheshire
-                                                                                   compojure
-                                                                                   org.slf4j/slf4j-api
-                                                                                   ch.qos.logback/logback-classic
-                                                                                   gorilla-renderable
-                                                                                   org.clojure/data.codec
-                                                                                   javax.servlet/servlet-api
-                                                                                   grimradical/clj-semver
-                                                                                   clojure-complete]
-                                                 ]]
-                                 :plugins      [[lein-gorilla "0.3.4"]]
-                                 :repl-options {:nrepl-handler sparkling.nrepl/handler}}}
-            :javac-options ["-Xlint:unchecked" "-source" "1.6" "-target" "1.6"]
-            :jvm-opts ^:replace ["-server" "-Xmx1g"]
-            :global-vars {*warn-on-reflection* false}
-            )
+  :aot [#".*" sparkling.serialization sparkling.destructuring]
+  :main tf-idf.core
+  :plugins [[lein-gorilla "0.3.6"]]
+  :profiles {:provided {:dependencies
+                        [[org.apache.spark/spark-core_2.10 "1.6.1"
+                          ;need exclusions thanks to this issue
+                          ;https://issues.apache.org/jira/browse/SPARK-1693
+                          :exclusions [[javax.servlet/servlet-api]
+                                       [org.eclipse.jetty.orbit/javax.servlet]]]
+                         [org.apache.spark/spark-mllib_2.10 "1.6.1"
+                          :exclusions [[javax.servlet/servlet-api]]]
+                          ]}
+             :dev {:plugins [[lein-dotenv "RELEASE"]]}})
+
 
 ;; run example with
-;;     lein run
+;;     lein gorilla-repl :port 9000
 
-
-
-
-;; First: Start an nrepl-server
-;; lein do clean, with-profile +gorilla repl :headless :port 9001
-
-;; Then, start a gorilla repl
-;; lein with-profile +gorilla gorilla :nrepl-port 9001  :port 9000
-
-
-;; and open the Gorilla REPL in your browser
-;; open http://127.0.0.1:9000/worksheet.html?filename=easy-report
+;; test with
+;;     lein test
