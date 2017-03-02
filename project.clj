b7a0b30f3faf44c1adaa71083eb22f514aa87a97
Dr. Christian Betz <cb@cypp.hamburg>
2017-03-02 12:27:31 +0100

    migrating to sparkling-2.0.0

--- project.clj
+++ project.clj
@@ -1,14 +1,15 @@
-(defproject gorillalabs/sparkling-getting-started "1.0.0-SNAPSHOT"
+(defproject gorillalabs/sparkling-getting-started "2.0.0-SNAPSHOT"
             :description "A Sample project demonstrating the use of Sparkling (https://gorillalabs.github.io/sparkling/), as shown in tutorial https://gorillalabs.github.io/sparkling/articles/tfidf_guide.html"
             :url "https://gorillalabs.github.io/sparkling/articles/tfidf_guide.html"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.8.0"]
-                           [gorillalabs/sparkling "1.2.4"]]
+                           [gorillalabs/sparkling "2.0.0-SNAPSHOT"]]
 
             :aot [#".*" sparkling.serialization sparkling.destructuring]
             :main tf-idf.core
-            :profiles {:provided {:dependencies [[org.apache.spark/spark-core_2.10 "1.5.1"]]}
+            :profiles {:provided {:dependencies [[org.apache.spark/spark-core_2.10 "2.1.0"]
+                                                 [org.apache.spark/spark-sql_2.10 "2.1.0"]]}
                        :dev {:plugins [[lein-dotenv "RELEASE"]]}})
 
 
